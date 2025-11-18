70d7b5a7039fc79ce72e6c01842422aaf304ef64
Muhammed Kazi <56946257+muhammednkazi@users.noreply.github.com>
2025-11-18 15:23:04 +0000

    updated day6

---
+++ Day-06/02-Assignment/01-Questions/ass1.py
@@ -0,0 +1,22 @@
+def add(num1, num2):
+    result=num1+num2
+    return result
+
+def sub(num1, num2):
+    result=num1-num2
+    return result
+
+def mul(num1, num2):
+    result=num1*num2
+    return result
+
+def div(num1, num2):
+    result=num1/num2
+    return result
+
+a=10
+b=5
+print(f"Addition is {add(a,b)}")
+print(f"Subtraction is {sub(a,b)}")
+print(f"Multiplication is {mul(a,b)}")
+print(f"Division is {div(a,b)}")
